2baf42d4d6e31d564cee86af49715fb6e626c2cd
Alexey Korepanov <alexkorep@gmail.com>
2022-08-26 19:49:30 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Phaser Snake game
+
+
+import Phaser from 'phaser';
+import logoImg from './assets/logo.png';
+
+class MyGame extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
+    {
+        this.load.image('logo', logoImg);
+        // Load player sprite
+        // this.load.spritesheet('player', 'assets/player.png', { frameWidth: 16, frameHeight: 16 });
+    }
+      
+    create ()
+    {
+        const logo = this.add.image(400, 150, 'logo');
+      
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    scene: MyGame
+};
+
+const game = new Phaser.Game(config);
